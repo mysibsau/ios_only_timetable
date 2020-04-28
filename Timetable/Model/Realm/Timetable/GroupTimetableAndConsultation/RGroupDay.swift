74c260970df0384084c9aaa5035d1972bd41d258
Artem Rylov <tema2707@icloud.com>
2020-04-28 19:30:58 +0700

    Добавил классы для расписания и консультаций групп

---
+++ Timetable/Model/Realm/Timetable/GroupTimetableAndConsultation/RGroupDay.swift
@@ -0,0 +1,17 @@
+//
+//  RGroupDay.swift
+//  Timetable
+//
+//  Created by art-off on 28.04.2020.
+//Copyright © 2020 art-off. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RGroupDay: Object {
+    
+    @objc dynamic var number = 0
+    let lessons = List<RGroupLesson>()
+    
+}
